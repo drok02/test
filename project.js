38c6d71361af47f89151468d0a380cfde5fbb6ed
이봉훈 <drok02@naver.com>
2021-12-14 16:50:56 +0900

    오타 찾기

--- project.js
+++ project.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
 	$("#imageForm").submit(function (event) {
-	
+
 	// Write your jQuery code in this area !
 	// You must use 3 APIs which is FileReader, FormData and jQuery.ajax() !
 	event.preventDefault();
-	var i=0;
-	var len=$("#images")[0].files.length;
+	var i= 0; var len =$("#images")[0].files.length;
 	var img, reader, file;
-	if(window.FormData){
+	if (window.FormData) {
 		formdata=new FormData();
 	}
-	for(;i<len; i++){
-		file=$("#images")[0].files[i];
-		if(file.type.match(/image.*/)){
-			if(window.FileReader){
-				reader=new FileReader();
-				reader.onloadend=function (e) {showUploadedItem(e.target.result);};
+	for (; i<len; i++){
+		file = $("#images")[0].files[i];
+		if(!!file.type.match(/image.*/)){
+			if(window.FileReader) {
+				reader = new FileReader();
+				reader.onloadend = function(e){
+					showUploadedItem(e.target.result);
+				};
 				reader.readAsDataURL(file);
 			}
-			if (formdata){
+			if(formdata){
 				formdata.append("images[]",file);
 			}
 		}
-		
 	}
-	if(formdata){
+	if (formdata){
 		$.ajax({
-			url:"upload.php",
+			url: "upload.php",
 			type: "POST",
 			data: formdata,
 			processData: false,
 			contentType: false,
 			success: function(res){
-				$("#response").html("Uploading Done.")
+				$("#response").html("Uploading Done.");
 			}
-		})
+		});
 	}
 	});
-});  
+}); 
